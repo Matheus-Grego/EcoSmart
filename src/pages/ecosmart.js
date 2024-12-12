7d9b1eb7af59ecfc76eadf52042b0c7f63e8abfe
Matheus Grego <137663305+Matheus-Grego@users.noreply.github.com>
2024-12-12 15:55:42 -0300

    Update ecosmart.js

--- src/pages/ecosmart.js
+++ src/pages/ecosmart.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import Fuse from "fuse.js";
 import "../css/ecosmart.css"
-import { PowerBIEmbed } from 'powerbi-client-react';
-import { Backdrop, Flex } from '@patternfly/react-core';
-import { Bold } from 'lucide-react';
-import { color } from 'chart.js/helpers';
+import { Separacao } from './EcoSmartTexts';
+import { Beneficios } from './EcoSmartTexts';
 
 
-const Modal = ({ isOpen, onClose }) => {
+const Modal = ({ isOpen, onClose, text }) => {
     if (!isOpen) return null;
     
 
@@ -16,52 +13,7 @@ const Modal = ({ isOpen, onClose }) => {
       <div style={styles.modalOverlay}>
         <div style={styles.modalContent}>
           <a onClick={onClose} style={styles.closeButton}>Fechar</a>
-          <div style={styles.modalText}>
-            <div className='title'>Separacão de residos</div>
-            <br></br>
-            <div>Uma das etapas mais importantes da coleta seletiva é separar corretamente os materiais recicláveis, orgânicos e rejeitos. Aqui está um guia prático e detalhado para ajudar: <span>PASSO A PASSO PARA SEPARAR RESÍDUOS</span></div> 
-            <ol>
-              <li>Tenha lixeiras ou sacos específicos para cada tipo de resíduo: Utilize recipientes diferentes, preferencialmente nas cores padrão da coleta seletiva (veja abaixo), para evitar a mistura de materiais. </li>
-              <li>Lave os recicláveis antes de descartar: Embalagens de alimentos e bebidas devem ser limpas para evitar odores e contaminação. Não é necessário lavar completamente, apenas remover o excesso de resíduos. </li>
-              <li>Dobre ou compacte materiais volumosos: Achate caixas de papelão, garrafas PET e latas de alumínio para economizar espaço.</li>
-              <li>Evite misturar recicláveis com rejeitos: Materiais recicláveis contaminados (como papéis engordurados) não podem ser reciclados.</li>
-            </ol>
-              
-              <div className='title'>Cores padrão das lixeiras</div>
-              <br></br>
-              <div>Essas cores são reconhecidas nacionalmente e ajudam na organização dos resíduos:</div>
-              <br></br>
-              <h3>Azul: Papel e papelão</h3>
-              <div>Exemplos: jornais, revistas, caixas de papelão, folhas de caderno.</div> 
-              <div>Evite: Papéis sujos, engordurados ou plastificados.</div>
-              <br></br>
-              <h3>Vermelho: Plásticos </h3>
-              <div>Exemplos: garrafas PET, embalagens de produtos de limpeza, sacolas plásticas.</div> 
-              <div>Evite: Embalagens metalizadas, plásticos misturados com outros materiais.</div>
-              <br></br>
-              <h3>Verde: Vidros</h3>
-              <div>Exemplos: garrafas, potes de vidro, frascos de perfumes.</div>
-              <div>Evite: Vidros quebrados, espelhos, lâmpadas fluorescentes. </div>
-              <br></br>
-              <h3>Amarelo: Metais</h3>
-              <div>Exemplos: latas de alumínio, tampas metálicas, pregos.</div>
-              <div>Evite: Metais contaminados, como latas de tinta ou óleo.</div>
-              <br></br>
-              <h3>Marrom: Resíduos orgânicos</h3>
-              <div>Exemplos: restos de alimentos, cascas de frutas e legumes.</div>
-              <div>Evite: Materiais recicláveis misturados com orgânicos.</div>
-              <br></br>
-              <h3>Preto: Rejeitos (não recicláveis)</h3> 
-              <div>Exemplos: absorventes, fraldas descartáveis, esponjas de aço.</div>
-              <br></br>
-              <div style={{color: "#7ead9a"}}>DICAS ADICIONAIS </div>
-              <br></br>
-              <div>Use etiquetas ou rótulos: Identifique os recipientes para facilitar a separação.</div>
-              <div>Compostagem: Aproveite resíduos orgânicos, como restos de comida, para fazer adubo caseiro.</div>
-              <div>Descarte materiais especiais: Pilhas, baterias, eletrônicos e lâmpadas precisam ser descartados em pontos específicos.</div>
-              
-              <div>Com uma separação eficiente, você contribui para a coleta seletiva e ajuda a preservar o meio ambiente!</div>
-            </div>
+          <div>{text}</div> 
         </div>
       </div>
     );
@@ -73,7 +25,7 @@ const Ecosmart = () => {
     const [dados, setDados] = useState(null)
     const [Search, setSearch] = useState(null)
     const [isModalOpen, setIsModalOpen] = useState(false);
-
+    const [modalText, setModalText] = useState(null);
     const [suggestions, setSuggestions] = useState([]);
     const [trashcan, setTrash] = useState(null);
 
@@ -86,13 +38,15 @@ const Ecosmart = () => {
       
     
     // Função para abrir o modal
-    const openModal = () => {
+    const openModal = (text) => {
+      setModalText(text)
       setIsModalOpen(true);
     };
   
     // Função para fechar o modal
     const closeModal = () => {
       setIsModalOpen(false);
+      setModalText(null);
     };
 
 
@@ -120,66 +74,79 @@ const Ecosmart = () => {
           "papelão",
           "jornal",
           "revista",
-          "caixa",
+          "caixa de papel",
           "embrulho",
           "caderno",
           "sulfite",
-          "livro"
+          "livro",
         ],
         "plastic": [
-          "embalagem",
+          "embalagem plástica",
+          "embalagem transparente",
           "garrafa plástica",
-          "frasco",
+          "plastica",
+          "plastico",
           "sacola",
           "tampa",
-          "copo",
-          "pote"
+          "copo plastico",
+          "pote",
+          "pacote"
         ],
         "glass": [
-          "garrafa",
-          "pote",
+          "garrafa de vidro",
+          "vidro",
+          "cacos",
+          "pote de vidro",
           "frasco",
-          "recipiente",
-          "copo"
+          "recipiente de vidro",
+          "copo de vidro"
         ],
         "metal": [
           "lata",
+          "latinha",
+          "aluminio",
           "garrafa de metal",
-          "conserva",
-          "tampa",
-          "cerveja",
-          "embalagem",
-          "ferro"
+          "grampo",
+          "metal"
         ],
+
         "organic": [
           "casca",
-          "borra",
-          "papel toalha"
+          "marmita",
+          "guardanapo sujo",
+          "papel toalha usado"
         ],
         "non-recycle": [
+          "adesivo",
+          "guardanapo limpo",
+          "sujeira",
+          "poeira",
+          "silicone",
           "embalagem laminada",
-          "salgadinho",
+          "embalagem de salgadinho",
           "sachê",
-          "pacote",
-          "metálica",
-          "alumínio",
-          "brilho"
+          "pacote laminado",
         ],
         "organic-food": [
-          "resto",
-          "legume",
+          "restos",
+          "alimentos",
+          "legumes",
+          "fruta",
+          "maça",
+          "massa",
+          "alimentos",
+          "vegetais",
           "semente",
           "carcaça",
-          "camarão",
           "osso"
         ]
       }]
       
-      const SearchData = () => {
+      const SearchData = (searchvalue) => {
         document.getElementsByClassName("search").value = ""
          trash.map(item => {
              for (const type of ['paper', 'plastic', 'glass','metal','organic','organic-food','non-recycle']) {
-               if (item[type].includes(Search)) {
+               if (item[type].includes(searchvalue)) {
                  document.getElementById(type).classList.add('active');
                  setTimeout(() => {
                      document.getElementById(type).classList.remove('active');
@@ -192,35 +159,39 @@ const Ecosmart = () => {
            }).filter(result => result !== null);
        }
 
+       const clickitem = (suggestion) => {
+        setSearch(suggestion)
+        SearchData(suggestion)
+       }
+
       const handleKeyPress = e => {
         if (e.key === "Enter") {
-          SearchData();
+          SearchData(Search);
         }
       };
 
       const handleSearchChange = (e) => {
-        const query = e.target.value;
-        setSearch(query);
-
-        // Filtra as palavras com base na entrada do usuário
+        
+        setSearch(e.target.value);
+        const query = e.target.value.replace(' ', ''); 
         if (query.length > 0) {
             // Junta todas as palavras de todas as categorias
             const allWords = trash[0] ? Object.values(trash[0]).flat() : [];
-            const filteredSuggestions = allWords.filter(word => word.toLowerCase().includes(query.toLowerCase()));
+            const filteredSuggestions = allWords.filter(word => (word.toLowerCase().includes(query) || query.includes(word))).slice(0,4);
+            --
             setSuggestions(filteredSuggestions);
-        } else {
+        } else {     
             setSuggestions([]);
         }
     };
 
     return(  
         <div className='Ecosmart'>
-
-            <Modal isOpen={isModalOpen} onClose={closeModal} />
+            <Modal isOpen={isModalOpen} onClose={closeModal} text={modalText}  />
              <div className='navbar'>
                 <a>Dados Reciclagem</a>
-                <a onClick={openModal}>Separacão de residos</a>
-                <a>Beneficios da coleta</a>
+                <a onClick={() => openModal(<Separacao />)}>Separacão de residos</a>
+                <a onClick={() => openModal(<Beneficios />)}>Beneficios da coleta</a>
                 <a>Educacão e dicas</a>
             </div>
             <div className='maincontainer'>
@@ -234,18 +205,19 @@ const Ecosmart = () => {
                       value={Search} 
                       onChange={handleSearchChange} 
                       onKeyPress={handleKeyPress} 
+                      style={{outline: "none"}}
                       placeholder="Pesquise por palavras..." 
                   />
-                    <img width={"20px"} src='/ecosmart-icons/search.png' onClick={SearchData} alt="Search" />
+                    <img width={"20px"} src='/ecosmart-icons/search.png' onClick={() => SearchData(Search)} alt="Search" />
                     
                 </div>
                 {suggestions.length > 0 && (
                         <ul className="suggestions-list">
                             {suggestions.map((suggestion, index) => (
-                                <li key={index} onClick={() => alert(suggestion)}>{suggestion}</li>
+                                <li key={index} onClick={() => clickitem(suggestion)}>{suggestion}</li>
                             ))}
                         </ul>
-                    )}
+                )}
                
             </div>
             <div className='cardscontainer'>
@@ -295,9 +267,7 @@ const Ecosmart = () => {
                             </div>
                             <img width={"100%"} src='/ecosmart-icons/plastic.png'></img>
                         </div>
-                        
-                    </div>
-                    
+                    </div>                  
                 </div>
                 
                 <div className='card' id="paper" style={{backgroundColor: "#5B9BFC"}}>
