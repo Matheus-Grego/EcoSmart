1ee77bcf470555dd9b8aba5e6746a07ec5af38d0
Matheus Grego <137663305+Matheus-Grego@users.noreply.github.com>
2024-12-12 15:56:54 -0300

    Create ecosmarttexts.js

---
+++ src/pages/ecosmarttexts.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export function Separacao() {
+    return(
+        <div className="modalText">
+            <div className='title'>Separacão de residos</div>
+            <br></br>
+            <div>Uma das etapas mais importantes da coleta seletiva é separar corretamente os materiais recicláveis, orgânicos e rejeitos. Aqui está um guia prático e detalhado para ajudar: <span>PASSO A PASSO PARA SEPARAR RESÍDUOS</span></div> 
+            <ol>
+              <li>Tenha lixeiras ou sacos específicos para cada tipo de resíduo: Utilize recipientes diferentes, preferencialmente nas cores padrão da coleta seletiva (veja abaixo), para evitar a mistura de materiais. </li>
+              <li>Lave os recicláveis antes de descartar: Embalagens de alimentos e bebidas devem ser limpas para evitar odores e contaminação. Não é necessário lavar completamente, apenas remover o excesso de resíduos. </li>
+              <li>Dobre ou compacte materiais volumosos: Achate caixas de papelão, garrafas PET e latas de alumínio para economizar espaço.</li>
+              <li>Evite misturar recicláveis com rejeitos: Materiais recicláveis contaminados (como papéis engordurados) não podem ser reciclados.</li>
+            </ol>
+              
+              <div className='title'>Cores padrão das lixeiras</div>
+              <br></br>
+              <div>Essas cores são reconhecidas nacionalmente e ajudam na organização dos resíduos:</div>
+              <br></br>
+              <h3>Azul: Papel e papelão</h3>
+              <div>Exemplos: jornais, revistas, caixas de papelão, folhas de caderno.</div> 
+              <div>Evite: Papéis sujos, engordurados ou plastificados.</div>
+              <br></br>
+              <h3>Vermelho: Plásticos </h3>
+              <div>Exemplos: garrafas PET, embalagens de produtos de limpeza, sacolas plásticas.</div> 
+              <div>Evite: Embalagens metalizadas, plásticos misturados com outros materiais.</div>
+              <br></br>
+              <h3>Verde: Vidros</h3>
+              <div>Exemplos: garrafas, potes de vidro, frascos de perfumes.</div>
+              <div>Evite: Vidros quebrados, espelhos, lâmpadas fluorescentes. </div>
+              <br></br>
+              <h3>Amarelo: Metais</h3>
+              <div>Exemplos: latas de alumínio, tampas metálicas, pregos.</div>
+              <div>Evite: Metais contaminados, como latas de tinta ou óleo.</div>
+              <br></br>
+              <h3>Marrom: Resíduos orgânicos</h3>
+              <div>Exemplos: restos de alimentos, cascas de frutas e legumes.</div>
+              <div>Evite: Materiais recicláveis misturados com orgânicos.</div>
+              <br></br>
+              <h3>Preto: Rejeitos (não recicláveis)</h3> 
+              <div>Exemplos: absorventes, fraldas descartáveis, esponjas de aço.</div>
+              <br></br>
+              <div style={{color: "#7ead9a"}}>DICAS ADICIONAIS </div>
+              <br></br>
+              <div>Use etiquetas ou rótulos: Identifique os recipientes para facilitar a separação.</div>
+              <div>Compostagem: Aproveite resíduos orgânicos, como restos de comida, para fazer adubo caseiro.</div>
+              <div>Descarte materiais especiais: Pilhas, baterias, eletrônicos e lâmpadas precisam ser descartados em pontos específicos.</div>
+              
+              <div>Com uma separação eficiente, você contribui para a coleta seletiva e ajuda a preservar o meio ambiente!</div>
+            </div>
+    )
+}
+
+export function Beneficios(){
+    return(
+        <div  className="modalText">
+            <div className='title'>Benefícios da Coleta Seletiva</div>
+            <br></br>
+            <div>A coleta seletiva vai além de uma simples prática de separação de resíduos. Ela é uma ferramenta poderosa para a preservação ambiental, geração de economia e transformação social. Ao adotar essa prática, você não só contribui para um planeta mais saudável, mas também colabora com a criação de uma economia mais sustentável. Descubra abaixo os principais benefícios de participar da coleta seletiva:</div>
+        </div>
+    )
+}
